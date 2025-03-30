6ed0152e5545231a2957749465bfbdbfd8f7b834
lelinnnn <leonardo2006santosgonca@gmail.com>
2025-03-30 19:49:07 -0300

    NumerosParesde1a100

---
+++ NumerosPares/numerospares/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numerospares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Números pares de 1 a 100:");
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
